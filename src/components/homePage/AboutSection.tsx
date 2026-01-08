709b1fbf10b6be6403a8f5bef8e0678b7a4a08e4
DibarpanDAS <187278257+Subham-cp@users.noreply.github.com>
2026-01-08 16:13:48 +0530

    Update AboutSection.tsx

--- src/components/homePage/AboutSection.tsx
+++ src/components/homePage/AboutSection.tsx
@@ -4,24 +4,24 @@ import { Section } from "@/components/ui/Section";
 import { WelcomeSection } from "./WelcomeSection";
 import { VisionSection } from "./VisionSection";
 import { TeamSpotlight } from "./TeamSpotlight";
-import { PresidentSpotlight } from "./PresidentSpotlight"; // Import the new component
+import { PresidentSpotlight } from "./PresidentSpotlight"; 
 import { MissionSection } from "./MissionSection";
 
 export function AboutSection() {
   return (
     <Section>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
-        {/* Left Column: Contains both Welcome and Mission */}
+        {/* Left Column: Contains Welcome, Mission, and Vision */}
         <div className="lg:col-span-2 flex flex-col gap-8">
           <WelcomeSection />
           <MissionSection />
+          <VisionSection /> {/* Moved here from the right column */}
         </div>
         
         {/* Right Column: Team Spotlight and President Spotlight */}
         <div className="lg:col-span-1 flex flex-col gap-8">
           <TeamSpotlight />
-          <PresidentSpotlight /> {/* Added here */}
-          <VisionSection />
+          <PresidentSpotlight />
         </div>
       </div>
     </Section>
